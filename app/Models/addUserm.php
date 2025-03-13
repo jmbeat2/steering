42e4e59f290400d46d2dd289fe67e468e72df3a3
JM <johnmichaelmancera@gmail.com>
2025-03-13 10:28:35 +0800

    added new functions for models
    Re-create the models function

--- app/Models/addUserm.php
+++ app/Models/addUserm.php
@@ -5,7 +5,27 @@ use CodeIgniter\Model;
 
 class addUserm extends Model
 {
-    protected $table = 'user'; // Your users table name
-    protected $primaryKey = 'id';
-    protected $allowedFields = ['name', 'firstname', 'middlename', 'lastname', 'shift_type', 'date'];
+    protected $table = 'users'; // Table name
+    protected $primaryKey = 'id'; // Primary key column
+
+    protected $allowedFields = [
+        'employee_id', 'fname', 'mname', 'lname', 'fullname', 'username', 'user_type', 'id_shift', 'password'
+    ]; 
+
+    protected $useTimestamps = true; // If using timestamps
+    protected $createdField  = 'created_at'; // Created field
+    protected $updatedField  = 'updated_at'; // Updated field
+
+    public function updateUser($id, $data)
+    {
+        // Ensure 'fullname' is updated with the correct concatenation of name fields
+        $data['fullname'] = $data['fname'] . ' ' . $data['mname'] . ' ' . $data['lname'];
+    
+        // Now use the built-in update() method to update the record
+        return $this->update($id, $data); // $id is the user ID, $data is the data array with updated values
+    }
+    
 }
+
+
+
